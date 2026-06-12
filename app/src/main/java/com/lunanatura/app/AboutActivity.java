1d602c92d4f3ab3a1f9ca1cfe7005842797a68bf
federicopizzarotti-source <federicopizzarotti@gmail.com>
2026-06-12 11:53:25 +0200

    Add files via upload

---
+++ app/src/main/java/com/lunanatura/app/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.lunanatura.app;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> finish());
+    }
+}
