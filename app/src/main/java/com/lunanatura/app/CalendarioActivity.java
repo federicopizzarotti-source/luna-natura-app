9118cdd809e2ed613c3af0bc04a582c472fe903d
federicopizzarotti-source <federicopizzarotti@gmail.com>
2026-06-12 13:07:47 +0200

    Update CalendarioActivity.java

--- app/src/main/java/com/lunanatura/app/CalendarioActivity.java
+++ app/src/main/java/com/lunanatura/app/CalendarioActivity.java
@@ -1,3 +1,26 @@
+package com.lunanatura.app;
+ 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+ 
+public class CalendarioActivity extends AppCompatActivity {
+ 
+    private TextView tvMeseAnno;
+    private GridLayout gridCalendario;
+    private Calendar calendarioCorrente;
+    private int oggiGiorno, oggiMese, oggiAnno;
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
